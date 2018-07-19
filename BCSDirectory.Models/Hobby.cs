8d4362d72a4939f3aaf6de18bcb462c1776e8de6
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-07-19 14:06:15 +0800

    Created models.

---
+++ BCSDirectory.Models/Hobby.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BCSDirectory.Models
+{
+    public class Hobby
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
