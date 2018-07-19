218128f00b8a05b3bc88307e2bc373bc2746b76a
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-07-19 19:42:27 +0800

    Updated DatePicker. Error in parsing date.

--- BCSDirectory/Controls/DatePicker.xaml.cs
+++ BCSDirectory/Controls/DatePicker.xaml.cs
@@ -1,28 +1,232 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BCSDirectory.Controls
 {
     /// <summary>
     /// Interaction logic for DatePicker.xaml
     /// </summary>
-    public partial class DatePicker : UserControl
+    public partial class DatePicker : UserControl, INotifyPropertyChanged
     {
         public DatePicker()
         {
             InitializeComponent();
+
+            Root.DataContext = this;
+
+            TwentyEightDays = new ObservableCollection<string>();
+            TwentyNineDays = new ObservableCollection<string>();
+            ThirtyDays = new ObservableCollection<string>();
+            ThirtyOneDays = new ObservableCollection<string>();
+
+            Months = new ObservableCollection<string>()
+            {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+            Month = Months[0];
+
+            Days = new ObservableCollection<string>();
+
+            for (int i = 0; i < 31; i++)
+            {
+                if (i < 28) TwentyEightDays.Add((i + 1) + "");
+                if (i < 29) TwentyNineDays.Add((i + 1) + "");
+                if (i < 30) ThirtyDays.Add((i + 1) + "");
+                ThirtyOneDays.Add((i + 1) + "");
+            }
+
+            Years = new ObservableCollection<string>();
+
+            for (int i = 0; i < 150; i++)
+            {
+                Years.Add((2018 - i) + "");
+            }
+
+            Year = Years[0];
+        }
+
+        public ObservableCollection<string> TwentyEightDays { get; set; }
+        public ObservableCollection<string> TwentyNineDays { get; set; }
+        public ObservableCollection<string> ThirtyDays { get; set; }
+        public ObservableCollection<string> ThirtyOneDays { get; set; }
+
+        #region MonthProperty
+
+        private string _month = "";
+
+        public string Month
+        {
+            get => _month;
+            set
+            {
+                if (_month != value)
+                {
+                    _month = value;
+                    OnPropertyChanged();
+                    UpdateDays();
+                    SetSelectedDate();
+                }
+            }
+        }
+
+        #endregion
+
+        #region DayProperty
+
+        private string _day;
+
+        public string Day
+        {
+            get => _day;
+            set
+            {
+                if (_day != value)
+                {
+                    _day = value;
+                    OnPropertyChanged();
+                    SetSelectedDate();
+                }
+            }
+        }
+
+        #endregion
+
+        #region YearProperty
+
+        private string _year;
+
+        public string Year
+        {
+            get => _year;
+            set
+            {
+                if (_year != value)
+                {
+                    _year = value;
+                    OnPropertyChanged();
+                    UpdateDays();
+                    SetSelectedDate();
+                }
+            }
+        }
+
+        #endregion
+
+        #region MonthsProperty
+
+        private ObservableCollection<string> _months;
+
+        public ObservableCollection<string> Months
+        {
+            get => _months;
+            set
+            {
+                _months = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region DaysProperty
+
+        private ObservableCollection<string> _days;
+
+        public ObservableCollection<string> Days
+        {
+            get => _days;
+            set
+            {
+                _days = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region YearsProperty
+
+        private ObservableCollection<string> _years;
+
+        public ObservableCollection<string> Years
+        {
+            get => _years;
+            set
+            {
+                _years = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        public DateTime Date
+        {
+            get { return (DateTime)GetValue(DateProperty); }
+            set { SetValue(DateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Date.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register("Date", typeof(DateTime), typeof(DatePicker), new PropertyMetadata(DateTime.Now));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateDays()
+        {
+            if (Month.Equals("February"))
+            {
+                Days = int.Parse(Year) % 4 == 0 ? TwentyNineDays : TwentyEightDays;
+            }
+            else if (Month.Equals("January") ||
+                     Month.Equals("March") ||
+                     Month.Equals("May") ||
+                     Month.Equals("July") ||
+                     Month.Equals("August") ||
+                     Month.Equals("October") ||
+                     Month.Equals("December"))
+            {
+                Days = ThirtyOneDays;
+            }
+            else
+            {
+                Days = ThirtyDays;
+            }
+        }
+
+        public void SetSelectedDate()
+        {
+           
+            try
+            {
+                Console.WriteLine(Years.IndexOf(Year) + "/" + int.Parse(Month) + "/" + int.Parse(Day));
+                Date = new DateTime(Years.IndexOf(Year), int.Parse(Month), int.Parse(Day));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine(Date);
         }
     }
 }
